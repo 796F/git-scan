d57fb12f198d6986883d35db04b53ee83f189f7c
zpalacios20 <zacharyp@stanford.edu>
2014-08-22 10:55:20 -0700

    small changes

--- models/Repositories.js
+++ models/Repositories.js
@@ -2,27 +2,30 @@ var https = require('https');
 
 Repositories = {
   getUsers : function () {
-  	var options = {
-	    host: 'api.github.com',
-	    path: '/search/repositories?q=+language:javascript+fork:false+created:>2014-01-01+pushed:>2014-06-01&per_page=100$page=2',
-	    method: 'GET',
-        headers: {'user-agent': 'node.js'}
-	};
+  	while(true) {
+	  	var options = {
+		    host: 'api.github.com',
+		    path: '/search/repositories?q=+language:javascript+fork:false+created:>2014-01-01+pushed:>2014-06-01&per_page=100$page=2',
+		    method: 'GET',
+	        headers: {'user-agent': 'node.js'}
+		};
 
-  	https.request(options, function(res) {
-	  var str = '';
+	  	https.request(options, function(res) {
+		  var str = '';
 
-	  res.on('data', function(d) {
-	    str += d;
-	  });
+		  res.on('data', function(d) {
+		    str += d;
+		  });
 
-	  res.on('end', function() {
-	  	var obj = JSON.parse(str);
-	  	console.log(obj);
-	  });
-	}).on('error', function(e) {
-	  console.error(e);
-	}).end();
+		  res.on('end', function() {
+		  	var obj = JSON.parse(str);
+		  	var items = obj.items;
+		  	console.log(obj);
+		  });
+		}).on('error', function(e) {
+		  console.error(e);
+		}).end();
+	}
   }
 }
 
