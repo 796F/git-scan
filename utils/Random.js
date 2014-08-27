ce5af3fb8707d317702a10b0f3b1dd356c224aad
Michael Xia <xia.umd@gmail.com>
2014-08-26 17:18:23 -0700

    working mysql insertion with examples

---
+++ utils/Random.js
@@ -0,0 +1,12 @@
+//useful later on maybe?  
+
+Random = {
+  id : function() { 
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  }  
+}
+
+module.exports = Random;
