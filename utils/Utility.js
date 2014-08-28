4e814616aabcc732fcc5f877a5e0c7cda22a10e5
Michael Xia <xia.umd@gmail.com>
2014-08-28 16:57:25 -0700

    utility for urls

---
+++ utils/Utility.js
@@ -0,0 +1,21 @@
+UTIL = {
+  buildUrlEncodedParameters : function (params){
+    return '?' + Object.keys(params).map(function(key){
+      return key + '=' + params[key];
+    }).join('&');
+  },
+  buildUrlWithPath : function (/* endpoint args */) {
+    var args = arguments;
+    return Object.keys(args).map(function(key, value, test) {
+      return args[key];
+    }).join('/');
+  },
+
+  buildGithubSearchQualifiers : function (qualifiers) {
+    return '+' + Object.keys(qualifiers).map(function(key) {
+      return key + ":" + qualifiers[key];
+    }).join('+');
+  }
+}
+  
+module.exports = UTIL;
